9321ea96847ed067c95ec5a2d3ed6a02c94f5406
glptrst <giulio.pietroiusti@gmail.com>
2022-06-30 00:09:46 +0200

    Sum pdfs pages.

--- scraper5.js
+++ scraper5.js
@@ -5,6 +5,92 @@ const jsdom = require('jsdom');
 const pdf = require('pdf-page-counter');
 const { JSDOM } = jsdom;
 
+// return page number of single pdf
+async function getPdfPages(path) {
+  return new Promise((resolve, reject) => {
+    let pdfLink = 'https://www.tdx.cat' + path;
+
+    if ( ! isValidUrl(pdfLink) )
+      throw new Error('???invalid url: ' + pdfLink);
+
+    if (pdfLink.indexOf('.jpg') !== -1)
+      throw new Error('??? jpg');
+
+    https.get(pdfLink, (res) => {
+      if (res.statusCode === 200) {
+        const filepath = './pdfs/' + pdfLink.replace('https://www.tdx.cat/', '').replace(/\//g, '_');
+        const file = fs.createWriteStream(filepath);
+        res.pipe(file);
+
+        file.on('finish', () => {
+          let dataBuffer = fs.readFileSync(filepath);
+          pdf(dataBuffer).then(data => {
+            resolve(data.numpages);
+          }).then(() => {
+            fs.unlink(filepath, (err) => {
+              if (err) throw err;
+              //console.log('File successfully removed: ' + filepath);
+            });
+          });
+        });
+
+        res.on('end', () => {
+          //??
+        });
+      } else {
+        throw new Error('??? not 200');
+      }
+    });
+  });  
+}
+
+// return sum of pages of several pdfs
+async function sumPdfPages(links) {
+  let total = 0;
+  for (let i = 0; i < links.length; i++) {
+    //console.log(links[i].firstElementChild.href);
+    let pdfPages = await getPdfPages(links[i].firstElementChild.href);
+    total += pdfPages;
+  }
+  return total;
+}
+
+async function getPages(thesisPage) {
+  return new Promise((resolve, reject) => {
+
+    const dom = new JSDOM(thesisPage);
+    const document = dom.window.document;
+
+    //throw new Error('this is my error');
+
+    let sivo = document.getElementsByClassName('simple-item-view-other');
+    let pages = null;
+    if (sivo.length !== 0) {
+      for (let i = 0; i < sivo.length; i++) {
+        let txt = (sivo[i].lastElementChild.textContent).trim();
+        //console.log(txt);
+        if (/^[0-9]+ p./.test(txt)) {
+          pages = txt;
+          pages = pages.slice(0, pages.length-3);
+          pages = Number(pages);
+          break;
+        }
+      }
+    }
+
+    if (pages) {
+      resolve(pages);
+    } else {
+      //console.log('doing the links ways');
+      let arr = [];
+      let fileLinks = document.getElementsByClassName('file-link');
+      //GET PDFS, SUM THEIR PAGE NUMBERS, AND RESOLVE PROMISE      
+      let total = sumPdfPages(fileLinks);
+      resolve(total);
+    }
+  });
+}
+
 function getThesis(path, thesisNum, nTry=0, e=null) {
   if (nTry >= 9) {
     let errorEntity = {path, thesisNum, nTry, error: e};
@@ -22,12 +108,14 @@ function getThesis(path, thesisNum, nTry=0, e=null) {
       res.setEncoding('utf8');
       let rawData = '';
       res.on('data', (chunk) => {rawData += chunk; });
-      res.on('end', () => {
-        const dom = new JSDOM(rawData);
-        const document = dom.window.document;
-        //...
-        let entity = {path, thesisNum, nTry};
-        appendToFile('./foobar_5.txt', entity);
+      res.on('end', async () => {
+        try {
+          let pages = await getPages(rawData);
+          let entity = {path, thesisNum, pages, nTry};
+          appendToFile('./foobar_5.txt', entity);
+        } catch(e) {
+          getThesis(path, thesisNum, nTry+1, e.message);
+        }
       });
     } else { // not 200
       getThesis(path, thesisNum, nTry+1, 'not 200');
@@ -36,6 +124,10 @@ function getThesis(path, thesisNum, nTry=0, e=null) {
     getThesis(path, thesisNum, nTry+1, e);
   });
 }
+
+//getThesis('/handle/10803/2257#page=12', 'foo');
+
+//getThesis('/handle/10803/283441', 'foo');
 //getThesis('/handle/10803/2257', 'foo');
 
 function getThesesFromList(path, listNum, nTry=0, e) {
@@ -94,12 +186,12 @@ function getThesesFromList(path, listNum, nTry=0, e) {
 //getThesesFromList('/discover?scope=&rpp=10&page=3&group_by=none&etal=0&ocult=0', 'foo');
 
 async function getAll() {
-  for (let i = 1; i < 7365; i++) {
+  for (let i = 1; i < 7372; i++) {
     //if (i % 3 == 0) {
     await sleep(1500);
     //}
     let path = 'https://www.tdx.cat/discover?locale-attribute=en&order=asc&rpp=5&sort_by=dc.date.issued_dt&page=' +i+ '&group_by=none&etal=0&ocult=0';
-    /* let path = 'https://www.tdx.cat/discover?locale-attribute=en&order=asc&rpp=5&sort_by=score&page=' +i+ '&group_by=none&etal=0&ocult=0'; */
+     //let path = 'https://www.tdx.cat/discover?locale-attribute=en&order=asc&rpp=5&sort_by=score&page=' +i+ '&group_by=none&etal=0&ocult=0';
     getThesesFromList(path, i, 0);
   }
 }
